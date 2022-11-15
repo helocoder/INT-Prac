55a5ee12b431680761d10669e24c3489b2245686
helocoder <vaidyaabhi3@gmail.com>
2022-11-15 20:55:10 +0530

    INT prac

---
+++ Javavscript/Calculator/func.js
@@ -0,0 +1,28 @@
+function add(){
+    var first= parseInt(document.getElementById("inp1").valueAsNumber);
+    var sec = parseInt(document.getElementById("inp2").valueAsNumber);
+    document.getElementById('res').value = first+sec;
+}
+
+function sub(){
+    var first= parseInt(document.getElementById("inp1").valueAsNumber);
+    var sec = parseInt(document.getElementById("inp2").valueAsNumber);
+    document.getElementById('res').value = first-sec;
+}
+
+function mul(){
+    var first= parseInt(document.getElementById("inp1").valueAsNumber);
+    var sec = parseInt(document.getElementById("inp2").valueAsNumber);
+    document.getElementById('res').value = first*sec;
+}
+
+function divi(){
+    var first= parseInt(document.getElementById("inp1").valueAsNumber);
+    var sec = parseInt(document.getElementById("inp2").valueAsNumber);
+    document.getElementById('res').value = first/sec;
+}
+
+
+
+
+
