ab3edfdee6f7c0caac42847a8e833b688026c69e
helocoder <vaidyaabhi3@gmail.com>
2022-11-19 18:24:34 +0530

    mouseOver

---
+++ Javavscript/Alert Msg/func.js
@@ -0,0 +1,8 @@
+function msg(){
+    var first_name = prompt('Enter First Name');
+    var last_name = prompt("Enter Last Name");
+
+    const full_name = first_name+" "+last_name;
+
+    alert("hellow "+full_name);
+}
