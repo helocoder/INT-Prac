b08555cc17195c94122f286c867554da13e9d5ee
helocoder <vaidyaabhi3@gmail.com>
2022-11-22 10:22:07 +0530

    node question 2

---
+++ Node/2question/file.js
@@ -0,0 +1,39 @@
+const file = require('fs');
+const data = file.readFileSync('./config.json');
+console.log(data.toString());
+//write//
+// var data = "Hello i,m content from file :)";
+// file.writeFile('sample.txt',data,(err)=>{
+//     if(err)
+//     {
+//         console.log(err)
+//     }
+//     else{
+
+//         console.log("file write success");
+//     }
+// });
+// //reading//
+// file.readFile('sample.txt',(err,data)=>{
+//     if(err)
+//     {
+//         console.log(err);
+//     }
+//     else{
+//         console.log(data.toString());
+
+//     }
+// })
+
+
+//reading json//
+// file.readFile(data,'utf-8',(err,data)=>{
+//     if(err)
+//     {
+//         console.log(err);
+//         return;
+//     }
+//     else{
+//         console.log(data);
+//     }
+// })
