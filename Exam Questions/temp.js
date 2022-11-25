4dc5467ebe8c3b4bdba7fe298f2445afdcb951c3
helocoder <vaidyaabhi3@gmail.com>
2022-11-26 00:39:07 +0530

    middleware

--- Exam Questions/temp.js
+++ Exam Questions/temp.js
@@ -1,27 +1,22 @@
-// make a http server in node and send res to client ?
-const fs = require('fs');
-const http = require('http');
-
-const options = {
-    hostname: '127.0.0.1',
-    port: 9500,
+// http in nodejs ?
+var options = {
+    host: 'localhost',
+    port: 7474,
+    path: '/db/data',
     method: 'GET',
-    path: '/_search',
     headers: {
-        'Content-Type': 'application/json'
+        accept: 'application/json'
     }
 };
 
-http.createServer((req, res) => {
-    req.pipe(fs.createWriteStream('reqBody.json'));
-
-    let request = http.request(options, (newRes) => {
-        newRes.pipe(res);
+console.log("Start");
+var x = http.request(options,function(res){
+    console.log("Connected");
+    res.on('data',function(data){
+        console.log(data);
     });
-    fs.createReadStream('reqBody.json').pipe(request);
+});
 
-    res.setHeader('Content-Type', 'application/json');
-
-}).listen(3000, '127.0.0.1');
+x.end();
 
 
